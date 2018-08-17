e69f596889b2c8b8d1b97936734458ece26e450b
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-08-17 11:14:39 -0400

    commit to command line

--- average specific number of scores/Program.cs
+++ average specific number of scores/Program.cs
@@ -9,45 +9,27 @@ namespace average_specific_number_of_scores
     class Program
     {
         static void Main(string[] args)
-            
+
         {
-            int numberOfTestScores;
+
+            int sum = 0;
             Console.WriteLine("Please enter the number of tests");
-            numberOfTestScores = int.Parse(Console.ReadLine());
-            
-            int number1;
-            int number2;
-            int number3;
-            int number4;
-            int number5;
-            int number6;
-            int number7;
-            int number8;
-            int number9;
-            int number10;
-            Console.WriteLine("Enter value 1");
-            number1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Value 2");
-            number2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value 3");
-            number3 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value 4");
-            number4 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Value 5");
-            number5 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value 6");
-            number6 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value 7");
-            number7 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Value 8");
-            number8 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value 9");
-            number9 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter value 10");
-            number10 = int.Parse(Console.ReadLine());
+            var numberOfTestScores = int.Parse(Console.ReadLine());
+            int[] num = new int[numberOfTestScores];
+
+            for (int i = 0; i < numberOfTestScores; i++)
+                do
+                {
+                    Console.WriteLine("Enter Score between 0 and 100: ");
+                   num[i] = int.Parse(Console.ReadLine());
+                } while (num [i] < 0 || num [i] > 100);
+
+            for (int i = 0; i < numberOfTestScores; i++)
+            {
+                sum += num[i];
+            }
             int avg;
-            avg = ((number1 + number2 + number3 + number4 + number5 +
-                      number6 + number7 + number8 + number9 + number10) / 10);
+            avg = sum / numberOfTestScores;
             Console.WriteLine("The average score is : {0}", avg);
             if ((avg >= 90) && (avg <= 100))
                 Console.WriteLine("The letter grade is an A");
